861410c81a64f4cc433f3aa321251f6e8e88ff60
Igor Pachkin <altair1901@icloud.com>
2024-03-07 17:46:08 +0100

    feat: initial project setup

--- Neobis_iOS_Auth/View/LoginViewController.swift
+++ Neobis_iOS_Auth/View/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Neobis_iOS_Auth
 //
 //  Created by Игорь Пачкин on 7/3/24.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
 
 
