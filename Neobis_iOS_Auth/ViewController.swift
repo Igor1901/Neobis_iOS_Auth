156c53546ee038e24762883ac5fc6184116e3ca3
Igor Pachkin <altair1901@icloud.com>
2024-03-07 17:36:45 +0100

    first commit

---
+++ Neobis_iOS_Auth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_Auth
+//
+//  Created by Игорь Пачкин on 7/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
